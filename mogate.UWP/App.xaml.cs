56d5a63d536f1803c9864624c2b6c229d80d057b
Sergii Rustamov <sergii.rustamov@gmail.com>
2018-02-20 16:41:16 +0200

    code clean

--- mogate.UWP/App.xaml.cs
+++ mogate.UWP/App.xaml.cs
@@ -59,15 +59,15 @@ namespace mogate.UWP
         return deviceFamily == "Windows.Xbox";
     }
 
-    /// <summary>
-    /// Invoked when the application is launched normally by the end user.  Other entry points
-    /// will be used such as when the application is launched to open a specific file.
-    /// </summary>
-    /// <param name="e">Details about the launch request and process.</param>
-    protected override void OnLaunched(LaunchActivatedEventArgs e)
-    {
-        // By default we want to fill the entire core window.
-        ApplicationView.GetForCurrentView().SetDesiredBoundsMode(ApplicationViewBoundsMode.UseCoreWindow);
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            // By default we want to fill the entire core window.
+            ApplicationView.GetForCurrentView().SetDesiredBoundsMode(ApplicationViewBoundsMode.UseCoreWindow);
 
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
@@ -76,45 +76,43 @@ namespace mogate.UWP
             }
 #endif
 
-        Frame rootFrame = Window.Current.Content as Frame;
+            Frame rootFrame = Window.Current.Content as Frame;
 
-        // Do not repeat app initialization when the Window already has content,
-        // just ensure that the window is active
-        if (rootFrame == null)
-        {
-            // Create a Frame to act as the navigation context and navigate to the first page
-            rootFrame = new Frame();
-
-            rootFrame.NavigationFailed += OnNavigationFailed;
-
-            if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
             {
-                //TODO: Load state from previously suspended application
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
             }
 
-            // Place the frame in the current Window
-            Window.Current.Content = rootFrame;
-        }
+            if (rootFrame.Content == null)
+            {
+                // When the navigation stack isn't restored navigate to the first page,
+                // configuring the new page by passing required information as a navigation
+                // parameter
+                rootFrame.Navigate(typeof(GamePage), e.Arguments);
+            }
 
-        if (rootFrame.Content == null)
-        {
-            // When the navigation stack isn't restored navigate to the first page,
-            // configuring the new page by passing required information as a navigation
-            // parameter
-            rootFrame.Navigate(typeof(GamePage), e.Arguments);
-        }
             // Ensure the current window is active
             float DPI = Windows.Graphics.Display.DisplayInformation.GetForCurrentView().LogicalDpi;
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
 
-            Windows.UI.ViewManagement.ApplicationView.PreferredLaunchWindowingMode = Windows.UI.ViewManagement.ApplicationViewWindowingMode.PreferredLaunchViewSize;
-
-            var desiredSize = new Windows.Foundation.Size(((float)1024 * 96.0f / DPI), ((float)768 * 96.0f / DPI));
-
-            Windows.UI.ViewManagement.ApplicationView.PreferredLaunchViewSize = desiredSize;
+            var desiredSize = new Size(((float)1024 * 96.0f / DPI), ((float)768 * 96.0f / DPI));
+            ApplicationView.PreferredLaunchViewSize = desiredSize;
 
             Window.Current.Activate();
-
-            bool result = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().TryResizeView(desiredSize);
+            ApplicationView.GetForCurrentView().TryResizeView(desiredSize);
         }
 
     /// <summary>
